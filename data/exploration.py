c639b654ced4853482072a01ae78f8bf6e09d49f
James Braza <james.braza@synthego.com>
2022-04-20 23:47:35 -0700

    Made exploration.py more amenable to importing

--- data/exploration.py
+++ data/exploration.py
@@ -1,18 +1,21 @@
+import os.path
+
 import pandas as pd
 
-FULL_CSV_FILE = "clothing_dataset_full/images.csv"
-SHIRTS_CSV_FILE = "shirts_dataset/Dataset/data.csv"
+DIR_ABS_PATH = os.path.dirname(os.path.abspath(__file__))
+FULL_CSV_REL_PATH = os.path.join("clothing_dataset_full", "images.csv")
+SHIRTS_CSV_REL_PATH = os.path.join("shirts_dataset", "Dataset", "data.csv")
 
 
 def explore_full_dataset() -> None:
-    data: pd.DataFrame = pd.read_csv(FULL_CSV_FILE)
+    data: pd.DataFrame = pd.read_csv(FULL_CSV_REL_PATH)
     print(data.info())
     all_labels = set(data["label"])
     counts_labels = data["label"].value_counts(sort=True)
 
 
 def explore_shirts_dataset() -> None:
-    data: pd.DataFrame = pd.read_csv(SHIRTS_CSV_FILE)
+    data: pd.DataFrame = pd.read_csv(SHIRTS_CSV_REL_PATH)
     print(data.info())
     all_types = set(data["Type"])
     counts_types = data["Type"].value_counts(sort=True)
