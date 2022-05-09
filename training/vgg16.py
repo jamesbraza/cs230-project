621904e6f269627bf416a177baf0616551823c60
James Braza <james.braza@synthego.com>
2022-05-08 21:44:25 -0700

    Added model checkpoint callback and updated gitignore

--- training/vgg16.py
+++ training/vgg16.py
@@ -6,7 +6,7 @@ from data.make import get_dataset, get_num_classes, make_vgg_preprocessing_gener
 from models.vgg16 import VGG_IMAGE_SIZE, make_tl_model
 
 NUM_EPOCHS = 10
-LOG_DIR_ABS_PATH = os.getcwd()
+LOG_DIR_ABS_PATH = os.path.join(os.getcwd(), "logs")
 
 # 1. Prepare the training data
 train_ds, _, _ = get_dataset("small", image_size=VGG_IMAGE_SIZE)
@@ -19,13 +19,14 @@ model = make_tl_model(num_classes)
 model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
 # 3. Fit the training data
-tensorboard_callback = tf.keras.callbacks.TensorBoard(
-    log_dir=LOG_DIR_ABS_PATH, histogram_freq=1
-)
+callbacks: list[tf.keras.callbacks.Callback] = [
+    tf.keras.callbacks.TensorBoard(log_dir=LOG_DIR_ABS_PATH, histogram_freq=1),
+    tf.keras.callbacks.ModelCheckpoint(LOG_DIR_ABS_PATH),
+]
 model.fit(
     train_data_generator,
     epochs=NUM_EPOCHS,
     steps_per_epoch=steps_per_epoch,
-    callbacks=[tensorboard_callback],
+    callbacks=[callbacks],
 )
 _ = 0
