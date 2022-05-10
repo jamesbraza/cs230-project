1a185a3e5e57716e9ee234071e9ee646abbb22e4
James Braza <james.braza@synthego.com>
2022-05-09 19:51:10 -0700

    Moved to save_best_only within ModelCheckpoint

--- training/vgg16_train.py
+++ training/vgg16_train.py
@@ -26,12 +26,11 @@ def make_vgg_preprocessing_generator(
 
 # 1. Prepare the training data
 train_ds, _, _ = get_dataset("small", image_size=VGG_IMAGE_SIZE)
-num_classes = get_num_classes(train_ds)
 train_data_generator = make_vgg_preprocessing_generator(train_ds, NUM_EPOCHS)
 steps_per_epoch: int = train_ds.cardinality().numpy()
 
 # 2. Create and compile the model
-model = make_tl_model(num_classes)
+model = make_tl_model(num_classes=get_num_classes(train_ds))
 model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
 # 3. Fit the training data
@@ -41,7 +40,7 @@ ckpt_filename = os.path.join(
 )
 callbacks: list[tf.keras.callbacks.Callback] = [
     tf.keras.callbacks.TensorBoard(log_dir=LOG_DIR_ABS_PATH, histogram_freq=1),
-    tf.keras.callbacks.ModelCheckpoint(ckpt_filename),
+    tf.keras.callbacks.ModelCheckpoint(ckpt_filename, save_best_only=True),
 ]
 model.fit(
     train_data_generator,
