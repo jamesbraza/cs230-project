db9bba3d1d9c09fa3e84028892c795c840063d21
James Braza <james.braza@synthego.com>
2022-05-15 21:02:36 -0700

    Refactored predict model to have a plotting helper function, and quantify train, val, test datasets

--- training/vgg16_predict.py
+++ training/vgg16_predict.py
@@ -1,6 +1,5 @@
 import os
 
-import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
 import tensorflow as tf
@@ -8,28 +7,44 @@ import tensorflow as tf
 from data.dataset_utils import get_dataset
 from models.vgg16 import VGG_IMAGE_SIZE
 from training import MODELS_DIR_ABS_PATH
+from training.utils import plot_batch_predictions
 
-MODEL_NAME = "first"
+MODEL_NAME = "2022-05-15T19-17-03.103670"
+PLOT_IMAGES = False
 
 
-# 1. Get the test data
-_, _, test_ds = get_dataset("small", image_size=VGG_IMAGE_SIZE, batch_size=16)
+def get_dataset_accuracy(
+    model: tf.keras.Model, dataset: tf.data.Dataset, plot_images: bool = PLOT_IMAGES
+) -> float:
+    """Get the prediction accuracy given a trained model and a dataset."""
+    num_correct, total = 0, 0
+    for batch_images, batch_labels in dataset:
+        preds: npt.NDArray[np.float32] = model.predict(batch_images)
+        labels_preds: list[tuple[int, int]] = [
+            (int(label), np.argmax(pred)) for label, pred in zip(batch_labels, preds)
+        ]
+        num_correct += sum(label == pred for label, pred in labels_preds)
+        total += len(labels_preds)
+        if plot_images:
+            images_labels_preds: list[tuple] = [
+                (image, *label_pred)
+                for image, label_pred in zip(batch_images, labels_preds)
+            ]
+            plot_batch_predictions(images_labels_preds, dataset.class_names)
+    return num_correct / total
+
+
+# 1. Get the dataset(s)
+train_ds, val_ds, test_ds = get_dataset(
+    "small", image_size=VGG_IMAGE_SIZE, batch_size=16
+)
 
 # 2. Rehydrate the model
 model_location = os.path.join(MODELS_DIR_ABS_PATH, MODEL_NAME)
 model = tf.keras.models.load_model(model_location)
 
-# 3. Make predictions on the entire test dataset
-for batch_images, batch_labels in test_ds:
-    preds: npt.NDArray[np.float] = model.predict(batch_images)
-    fig, ax = plt.subplots(nrows=4, ncols=4)
-    for i, image in enumerate(batch_images):
-        fig.axes[i].imshow(image.numpy().astype("uint8"))
-        pred_label = np.argmax(preds[i])
-        fig.axes[i].set_title(
-            f"Label: {int(batch_labels[i])}: {test_ds.class_names[batch_labels[i]]}\n"
-            f"Pred: {pred_label}: {test_ds.class_names[pred_label]}"
-        )
-    fig.tight_layout()  # Prevent title overlap
-    plt.show()
-    _ = 0  # Debug here
+# 3. Make predictions on the entire dataset
+train_set_accuracy = get_dataset_accuracy(model, train_ds)
+val_set_accuracy = get_dataset_accuracy(model, val_ds)
+test_set_accuracy = get_dataset_accuracy(model, test_ds)
+_ = 0  # Debug here
