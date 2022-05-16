31832a926164898befd9b9dad0b095cf03859c00
James Braza <james.braza@synthego.com>
2022-05-15 21:27:54 -0700

    Even better training (#14)
    * Made the number of Dense layers configurable
    
    * Moved from 50, 50, 20 to 64, 64, 16 to be cheeky
    
    * Moved from dev naming to validation naming
    
    * Added validation data/steps, preprocessing option, changed FC units
    
    * Refactored predict model to have a plotting helper function, and quantify train, val, test datasets
    
    * Added printout for training, validation, and test set accuracy

--- training/utils.py
+++ training/utils.py
@@ -1,6 +1,31 @@
+import math
 from datetime import datetime
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
 
 
 def get_ts_now_as_str() -> str:
     """Get an ISO 8601-compliant timestamp for use in naming."""
     return datetime.now().isoformat().replace(":", "-")
+
+
+def plot_batch_predictions(
+    images_labels_preds: list[tuple[tf.Tensor, int, int]], class_names: list[str]
+) -> None:
+    batch_size = len(images_labels_preds)
+    subplot_size = int(math.sqrt(batch_size))
+    if subplot_size**2 != batch_size:
+        raise NotImplementedError(
+            f"Only works for perfect-square batch sizes, "
+            f"not a batch size of {batch_size}."
+        )
+    fig, ax = plt.subplots(nrows=subplot_size, ncols=subplot_size)
+    for i, (image, label, pred) in enumerate(images_labels_preds):
+        fig.axes[i].imshow(image.numpy().astype("uint8"))
+        fig.axes[i].set_title(
+            f"Label: {label}: {class_names[label]}\n"
+            f"Pred: {pred}: {class_names[pred]}"
+        )
+    fig.tight_layout()  # Prevent title overlap
+    plt.show()
--- training/vgg16_train.py
+++ training/vgg16_train.py
@@ -1,5 +1,5 @@
 import os
-from collections.abc import Iterator
+from collections.abc import Iterable
 
 import numpy.typing as npt
 import tensorflow as tf
@@ -10,25 +10,40 @@ from training import CKPTS_DIR_ABS_PATH, LOG_DIR_ABS_PATH, MODELS_DIR_ABS_PATH
 from training.utils import get_ts_now_as_str
 
 NUM_EPOCHS = 10
+VALIDATION_STEPS = 3
 
 
 def make_vgg_preprocessing_generator(
-    dataset: tf.data.Dataset, num_epochs: int
-) -> Iterator[tuple[tf.Tensor, npt.NDArray[tf.bool]]]:
-    """Make an iterator that pre-processes a dataset for VGGNet training."""
+    dataset: tf.data.Dataset, num_epochs: int, preprocess_image: bool = False
+) -> Iterable[tuple[tf.Tensor, npt.NDArray[tf.bool]]]:
+    """
+    Make an iterator that pre-processes a dataset for VGGNet training.
+
+    Args:
+        dataset: TensorFlow dataset to preprocess.
+        num_epochs: Count of training epochs.
+        preprocess_image: Set True to pre-process the image per VGG16's preprocessor.
+            Default is False because this is built into the model.
+
+    Returns:
+        Iterable of (image, categorical label) tuples.
+    """
     for batch_images, batch_labels in dataset.repeat(num_epochs):
+        if preprocess_image:
+            batch_images = tf.keras.applications.vgg16.preprocess_input(batch_images)
         yield batch_images, tf.keras.utils.to_categorical(
             batch_labels, get_num_classes(dataset), dtype="bool"
         )
 
 
 # 1. Prepare the training data
-train_ds, _, _ = get_dataset("small", image_size=VGG_IMAGE_SIZE)
+train_ds, val_ds, _ = get_dataset("small", image_size=VGG_IMAGE_SIZE)
 train_data_generator = make_vgg_preprocessing_generator(train_ds, NUM_EPOCHS)
+val_data_generator = make_vgg_preprocessing_generator(val_ds, NUM_EPOCHS)
 steps_per_epoch: int = train_ds.cardinality().numpy()
 
 # 2. Create and compile the model
-model = make_tl_model(num_classes=get_num_classes(train_ds), top_fc_units=(50, 50, 20))
+model = make_tl_model(num_classes=get_num_classes(train_ds), top_fc_units=(64, 64, 16))
 model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
 # 3. Perform the actual training
@@ -45,8 +60,11 @@ model.fit(
     train_data_generator,
     epochs=NUM_EPOCHS,
     steps_per_epoch=steps_per_epoch,
+    validation_data=val_data_generator,
+    validation_steps=VALIDATION_STEPS,
     callbacks=[callbacks],
 )
 
 # 4. Save the model for future use
 model.save(os.path.join(MODELS_DIR_ABS_PATH, current_ts))
+_ = 0  # Debug here
