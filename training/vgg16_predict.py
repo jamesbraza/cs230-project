f8b2b5163236fb0d346847d8822409f78dfe254c
James Braza <james.braza@synthego.com>
2022-05-22 14:34:42 -0700

    Moved to more verbose prediction printing to help underscore improvements

--- training/vgg16_predict.py
+++ training/vgg16_predict.py
@@ -1,4 +1,6 @@
+import collections
 import os
+from typing import TypeAlias
 
 import numpy as np
 import numpy.typing as npt
@@ -12,26 +14,45 @@ from training.utils import plot_batch_predictions
 MODEL_NAME = "2022-05-15T19-17-03.103670"
 PLOT_IMAGES = False
 
+Statistics: TypeAlias = dict[tuple[int, str], tuple[int, int]]
 
-def get_dataset_accuracy(
+
+def get_dataset_predict_stats(
     model: tf.keras.Model, dataset: tf.data.Dataset, plot_images: bool = PLOT_IMAGES
-) -> tuple[float, int]:
-    """Get the prediction accuracy and image count using the passed model an dataset."""
-    num_correct, total = 0, 0
+) -> Statistics:
+    """Get the number correct and image count using the passed model and dataset."""
+    correct_totals = collections.defaultdict(lambda: [0, 0])
     for batch_images, batch_labels in dataset:
         preds: npt.NDArray[np.float32] = model.predict(batch_images)
         labels_preds: list[tuple[int, int]] = [
             (int(label), np.argmax(pred)) for label, pred in zip(batch_labels, preds)
         ]
-        num_correct += sum(label == pred for label, pred in labels_preds)
-        total += len(labels_preds)
+        for label, pred in labels_preds:
+            if label == pred:
+                correct_totals[label][0] += 1  # Num correct
+            correct_totals[label][1] += 1  # Total
         if plot_images:
             images_labels_preds: list[tuple] = [
                 (image, *label_pred)
                 for image, label_pred in zip(batch_images, labels_preds)
             ]
             plot_batch_predictions(images_labels_preds, dataset.class_names)
-    return num_correct / total, total
+    return {
+        (label, dataset.class_names[label]): (correct, total)
+        for label, (correct, total) in sorted(correct_totals.items())
+    }
+
+
+def get_dataset_accuracy(
+    stats: Statistics,
+) -> tuple[float, int, dict[tuple[int, str], float]]:
+    """Get the accuracy % and total image count from a statistics dict."""
+    num_correct, num_total, per_label_acc = 0, 0, {}
+    for label_tup, (correct, total) in stats.items():
+        num_correct += correct
+        num_total += total
+        per_label_acc[label_tup] = num_correct / num_total
+    return num_correct / num_total, num_total, per_label_acc
 
 
 # 1. Get the dataset(s)
@@ -44,15 +65,23 @@ model_location = os.path.join(MODELS_DIR_ABS_PATH, MODEL_NAME)
 model = tf.keras.models.load_model(model_location)
 
 # 3. Make predictions on the dataset(s)
-train_set_accuracy, num_train_images = get_dataset_accuracy(model, train_ds)
-val_set_accuracy, num_val_images = get_dataset_accuracy(model, val_ds)
-test_set_accuracy, num_test_images = get_dataset_accuracy(model, test_ds)
-print(
-    f"Training set accuracy: {train_set_accuracy * 100:.2f}% correct "
-    f"of {num_train_images} images{os.linesep}"
-    f"Validation set accuracy: {val_set_accuracy * 100:.2f}% correct "
-    f"of {num_val_images} images{os.linesep}"
-    f"Test set accuracy: {test_set_accuracy * 100:.2f}% correct "
-    f"of {num_test_images} images"
-)
+train_ds_stats = get_dataset_predict_stats(model, train_ds)
+train_ds_summary = get_dataset_accuracy(train_ds_stats)
+val_ds_stats = get_dataset_predict_stats(model, val_ds)
+val_ds_summary = get_dataset_accuracy(val_ds_stats)
+test_ds_stats = get_dataset_predict_stats(model, test_ds)
+test_ds_summary = get_dataset_accuracy(test_ds_stats)
+for ds_name, ds_accuracy, ds_total, ds_per_label in [
+    ("Training", *train_ds_summary),
+    ("Validation", *val_ds_summary),
+    ("Test", *test_ds_summary),
+]:
+    readable_ds_per_label = {
+        class_name: round(acc * 100, 2)
+        for (label, class_name), acc in ds_per_label.items()
+    }
+    print(
+        f"{ds_name} set accuracy: {ds_accuracy * 100:.2f}% correct "
+        f"of {ds_total} images: {readable_ds_per_label}."
+    )
 _ = 0  # Debug here
