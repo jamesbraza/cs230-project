3d9a8c70ca97989c4907f909d31f183ddb24c8a0
James Braza <james.braza@synthego.com>
2022-05-26 22:36:37 -0700

    Combined training files

--- training/vgg16_train.py
+++ training/vgg16_train.py
@@ -3,7 +3,14 @@ from typing import List, Optional
 
 import tensorflow as tf
 
-from data.dataset_utils import get_dataset, get_num_classes
+from data.dataset_utils import (
+    FULL_DATASET_LABELS,
+    SMALL_DATASET_LABELS,
+    get_dataset,
+    get_label_overlap,
+    get_num_classes,
+    pass_class_names,
+)
 from models.vgg16 import VGG_IMAGE_SIZE, make_tl_model
 from training import CKPTS_DIR_ABS_PATH, LOG_DIR_ABS_PATH, MODELS_DIR_ABS_PATH
 from training.utils import get_ts_now_as_str
@@ -16,6 +23,8 @@ ES_PATIENCE_EPOCHS = 8
 # Number of validation set batches to check after each epoch, set None to check
 # all validation batches
 VALIDATION_STEPS: Optional[int] = None
+# If you want to mix in the full clothing dataset
+DATA_AUGMENTATION = True
 
 
 # 1. Prepare the training data
@@ -23,6 +32,19 @@ train_ds, val_ds, _ = get_dataset("small", image_size=VGG_IMAGE_SIZE)
 train_ds = vgg_preprocess_dataset(train_ds)
 train_steps_per_epoch: Optional[int] = train_ds.cardinality().numpy()
 val_ds = vgg_preprocess_dataset(val_ds)
+if DATA_AUGMENTATION:
+    full_train_ds, _, _ = get_dataset(
+        "full",
+        image_size=VGG_IMAGE_SIZE,
+        validation_split=0.0,
+        filter_labels=get_label_overlap(
+            SMALL_DATASET_LABELS, other_ds_labels=FULL_DATASET_LABELS
+        ),
+    )
+    full_train_ds = vgg_preprocess_dataset(full_train_ds)
+    train_ds = pass_class_names(train_ds, train_ds.concatenate(full_train_ds))
+    if train_ds.cardinality().numpy() == tf.data.experimental.UNKNOWN_CARDINALITY:
+        train_steps_per_epoch = None
 if VALIDATION_STEPS is None:
     val_steps_per_epoch: int = val_ds.cardinality().numpy()
 else:
