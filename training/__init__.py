2aa7a67b990299a9477af11763aa64bc2ea705af
James Braza <james.braza@synthego.com>
2022-05-26 19:44:24 -0700

    Added forgotten dirname

--- training/__init__.py
+++ training/__init__.py
@@ -1,5 +1,5 @@
 import os.path
 
-LOG_DIR_ABS_PATH = os.path.join(__file__, "logs")
-CKPTS_DIR_ABS_PATH = os.path.join(__file__, "checkpoints")
-MODELS_DIR_ABS_PATH = os.path.join(__file__, "models")
+LOG_DIR_ABS_PATH = os.path.join(os.path.dirname(__file__), "logs")
+CKPTS_DIR_ABS_PATH = os.path.join(os.path.dirname(__file__), "checkpoints")
+MODELS_DIR_ABS_PATH = os.path.join(os.path.dirname(__file__), "models")
