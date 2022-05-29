71381173cebee3662b49a0d756dd53572e186c0d
James Braza <james.braza@synthego.com>
2022-05-28 18:04:34 -0700

    Enhanced VGG16 model and training

--- models/vgg16.py
+++ models/vgg16.py
@@ -4,39 +4,45 @@ from typing import Tuple
 
 import tensorflow as tf
 
-VGG_IMAGE_SIZE = (224, 224)
+TopFCUnits = Tuple[int, ...]
+
+VGG_IMAGE_SIZE = (224, 224)  # Channels last format
 VGG_IMAGE_SHAPE = (*VGG_IMAGE_SIZE, 3)  # RGB
-VGG_TOP_FC_UNITS = (4096, 4096, 1000)
-NUM_EPOCHS = 10
+VGG_TOP_FC_UNITS: TopFCUnits = (4096, 4096, 1000)  # From the paper to match ImageNet
 
 
-def make_tl_model(
-    num_classes: int, top_fc_units: Tuple[int, ...] = VGG_TOP_FC_UNITS
-) -> tf.keras.Model:
+def make_tl_model(top_fc_units: TopFCUnits = VGG_TOP_FC_UNITS) -> tf.keras.Model:
     """
     Make a VGG16 model given a number of classes and FC units.
 
     Args:
-        num_classes: Number of classes for the final softmax layer.
         top_fc_units: Number of units to use in each of the top FC layers.
             Default is three FC layers per the VGGNet paper.
+            Last value in the tuple should match your number of classes.
 
     Returns:
-        Model created.
+        VGGNet model created.
     """
     base_model = tf.keras.applications.VGG16(weights="imagenet", include_top=False)
     base_model.trainable = False  # Freeze the model
     dense_layers = [
-        tf.keras.layers.Dense(units, activation="relu") for units in top_fc_units
+        tf.keras.layers.Dense(units=units, activation="relu", name=f"fc{i+1}")
+        for i, units in enumerate(top_fc_units[:-1])
     ]
     return tf.keras.Sequential(
         [
             tf.keras.Input(shape=VGG_IMAGE_SHAPE),  # Specify input size
-            tf.keras.layers.Lambda(tf.keras.applications.vgg16.preprocess_input),
+            tf.keras.layers.Lambda(
+                function=tf.keras.applications.vgg16.preprocess_input,
+                name="preprocess_images",
+            ),
             base_model,
             tf.keras.layers.Flatten(),
             *dense_layers,
             # Last layer matches number of classes
-            tf.keras.layers.Dense(num_classes, activation="softmax"),
-        ]
+            tf.keras.layers.Dense(
+                units=top_fc_units[-1], activation="softmax", name="predictions"
+            ),
+        ],
+        name="diy_tl_vgg16",
     )
--- training/vgg16_train.py
+++ training/vgg16_train.py
@@ -1,5 +1,5 @@
 import os
-from typing import List, Optional
+from typing import Callable, List, Literal, Optional, Tuple, Union
 
 import tensorflow as tf
 
@@ -8,13 +8,11 @@ from data.dataset_utils import (
     SMALL_DATASET_LABELS,
     get_dataset,
     get_label_overlap,
-    get_num_classes,
     pass_class_names,
 )
-from models.vgg16 import VGG_IMAGE_SIZE, make_tl_model
+from models.vgg16 import VGG_IMAGE_SIZE, VGG_TOP_FC_UNITS, TopFCUnits, make_tl_model
 from training import CKPTS_DIR_ABS_PATH, LOG_DIR_ABS_PATH, MODELS_DIR_ABS_PATH
-from training.utils import get_ts_now_as_str
-from training.vgg16_utils import vgg_preprocess_dataset
+from training.utils import get_ts_now_as_str, preprocess_dataset
 
 # Num epochs if not early stopped
 MAX_NUM_EPOCHS = 64
@@ -28,26 +26,37 @@ DATA_AUGMENTATION = True
 # Set to the last checkpoint if you want to resume training,
 # or leave as None to begin anew
 LAST_CHECKPOINT: Optional[str] = None
+# Which model to train
+MODEL: Literal["vgg16"] = "vgg16"
 
+if MODEL == "vgg16":
+    image_size: Tuple[int, int] = VGG_IMAGE_SIZE
+    model_factory: Callable[..., tf.keras.Model] = make_tl_model
+    top_fc_units: Union[TopFCUnits, int] = (
+        *[u for u in VGG_TOP_FC_UNITS[:-1]],
+        len(SMALL_DATASET_LABELS),
+    )
+else:
+    raise NotImplementedError(f"Unrecognized model: {MODEL}.")
 
 # 1. Prepare the training data
-train_ds, val_ds, _ = get_dataset("small", image_size=VGG_IMAGE_SIZE)
-train_ds = vgg_preprocess_dataset(train_ds)
+train_ds, val_ds, _ = get_dataset("small", image_size=image_size)
+train_ds = preprocess_dataset(train_ds)
 train_steps_per_epoch: Optional[int] = train_ds.cardinality().numpy()
 if DATA_AUGMENTATION:
     full_train_ds, _, _ = get_dataset(
         "full",
-        image_size=VGG_IMAGE_SIZE,
+        image_size=image_size,
         validation_split=0.0,
         filter_labels=get_label_overlap(
             SMALL_DATASET_LABELS, other_ds_labels=FULL_DATASET_LABELS
         ),
     )
-    full_train_ds = vgg_preprocess_dataset(full_train_ds)
+    full_train_ds = preprocess_dataset(full_train_ds)
     train_ds = pass_class_names(train_ds, train_ds.concatenate(full_train_ds))
     if train_ds.cardinality().numpy() == tf.data.experimental.UNKNOWN_CARDINALITY:
         train_steps_per_epoch = None
-val_ds = vgg_preprocess_dataset(val_ds)
+val_ds = preprocess_dataset(val_ds)
 if VALIDATION_STEPS is None:
     val_steps_per_epoch: int = val_ds.cardinality().numpy()
 else:
@@ -57,9 +66,7 @@ train_ds = pass_class_names(train_ds, train_ds.prefetch(tf.data.AUTOTUNE))
 val_ds = pass_class_names(val_ds, val_ds.prefetch(tf.data.AUTOTUNE))
 
 # 2. Create and compile the model
-model = make_tl_model(
-    num_classes=get_num_classes(train_ds), top_fc_units=(128, 128, 32)
-)
+model = model_factory(top_fc_units=top_fc_units)
 model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
 # 3. Perform the actual training
