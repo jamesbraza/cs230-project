6a3296e84cfa05d12847e8c38c19a0ed615659a7
James Braza <james.braza@synthego.com>
2022-05-29 22:14:17 -0700

    Moved to helper method for nonshortcut part of ResNet

--- models/resnet_utils.py
+++ models/resnet_utils.py
@@ -4,30 +4,18 @@ import tensorflow as tf
 from tensorflow.python.keras.engine.keras_tensor import KerasTensor
 
 
-def make_identity_block(
+def _make_nonshortcut(
     x: KerasTensor,
     filters: Tuple[int, int, int],
-    stage: int,
-    block: int,
+    base_name: str,
+    first_strides: Tuple[int, int] = (1, 1),
 ) -> KerasTensor:
-    """
-    Make a "bottleneck" building block for a ResNet.
-
-    Args:
-        x: Model under construction to place the identity block on top of.
-        filters: Three-tuple of the three Conv2D's output dimensionality.
-        stage: Stage number for layer names.
-        block: Block number for layer names, where blocks are stage sub-parts.
-
-    Returns:
-        Further constructed model.
-    """
-    base_name = f"conv{stage}_block{block}"
-    x_nonshortcut = x_shortcut = x
+    """Build the core identity block's non-shortcut branch."""
     for layer in [
         tf.keras.layers.Conv2D(
             filters=filters[0],
             kernel_size=(1, 1),
+            strides=first_strides,
             name=f"{base_name}_1_conv",
         ),
         tf.keras.layers.BatchNormalization(axis=3, name=f"{base_name}_1_bn"),
@@ -47,7 +35,31 @@ def make_identity_block(
         ),
         tf.keras.layers.BatchNormalization(axis=3, name=f"{base_name}_3_bn"),
     ]:
-        x_nonshortcut = layer(x_nonshortcut)
+        x = layer(x)
+    return x
+
+
+def make_identity_block(
+    x: KerasTensor,
+    filters: Tuple[int, int, int],
+    stage: int,
+    block: int,
+) -> KerasTensor:
+    """
+    Make a "bottleneck" building block for a ResNet.
+
+    Args:
+        x: Model under construction to place the identity block on top of.
+        filters: Three-tuple of the three Conv2D's output dimensionality.
+        stage: Stage number for layer names.
+        block: Block number for layer names, where blocks are stage sub-parts.
+
+    Returns:
+        Further constructed model.
+    """
+    base_name = f"conv{stage}_block{block}"
+    x_shortcut = x
+    x_nonshortcut = _make_nonshortcut(x, filters, base_name)
     x = tf.keras.layers.Add(name=f"{base_name}_add")([x_shortcut, x_nonshortcut])
     return tf.keras.layers.Activation(activation="relu", name=f"{base_name}_out")(x)
 
@@ -73,7 +85,7 @@ def make_convolutional_block(
         Further constructed model.
     """
     base_name = f"conv{stage}_block{block}"
-    x_nonshortcut = x_shortcut = x
+    x_shortcut = x
     for layer in [
         tf.keras.layers.Conv2D(
             filters=filters[2],
@@ -84,30 +96,6 @@ def make_convolutional_block(
         tf.keras.layers.BatchNormalization(axis=3, name=f"{base_name}_0_bn"),
     ]:
         x_shortcut = layer(x_shortcut)
-    for layer in [
-        tf.keras.layers.Conv2D(
-            filters=filters[0],
-            kernel_size=(1, 1),
-            strides=first_strides,
-            name=f"{base_name}_1_conv",
-        ),
-        tf.keras.layers.BatchNormalization(axis=3, name=f"{base_name}_1_bn"),
-        tf.keras.layers.Activation(activation="relu", name=f"{base_name}_1_relu"),
-        tf.keras.layers.Conv2D(
-            filters=filters[1],
-            kernel_size=(3, 3),
-            padding="same",
-            name=f"{base_name}_2_conv",
-        ),
-        tf.keras.layers.BatchNormalization(axis=3, name=f"{base_name}_2_bn"),
-        tf.keras.layers.Activation(activation="relu", name=f"{base_name}_2_relu"),
-        tf.keras.layers.Conv2D(
-            filters=filters[2],
-            kernel_size=(1, 1),
-            name=f"{base_name}_3_conv",
-        ),
-        tf.keras.layers.BatchNormalization(axis=3, name=f"{base_name}_3_bn"),
-    ]:
-        x_nonshortcut = layer(x_nonshortcut)
+    x_nonshortcut = _make_nonshortcut(x, filters, base_name, first_strides)
     x = tf.keras.layers.Add(name=f"{base_name}_add")([x_shortcut, x_nonshortcut])
     return tf.keras.layers.Activation(activation="relu", name=f"{base_name}_out")(x)
